3b32da96591a625d0c22381585333fb162275028
종훈 <leejonghun>
2021-02-02 14:53:49 +0900

    add eslint

--- Front/Project/.eslintrc.js
+++ Front/Project/.eslintrc.js
@@ -1,17 +1,28 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+	// 현재 eslintrc 파일을 기준으로 ESLint 규칙을 적용
+	root: true,
+	// 추가적인 규칙들을 적용
+	extends: [
+		'eslint:recommended',
+		'plugin:vue/essential',
+		'prettier',
+		'plugin:prettier/recommended',
+	],
+	plugins: ['prettier'],
+	rules: {
+		'prettier/prettier': [
+			'error',
+			{
+				singleQuote: true,
+				semi: true,
+				useTabs: true,
+				tabWidth: 2,
+				trailingComma: 'all',
+				printWidth: 80,
+				bracketSpacing: true,
+				arrowParens: 'avoid',
+			},
+		],
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+	},
+};

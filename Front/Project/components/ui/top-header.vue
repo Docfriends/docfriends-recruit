01f0f287f549b1f17497a1d3544e106a34e29239
종훈 <leejonghun>
2021-02-02 14:54:02 +0900

    haeder indent

--- Front/Project/components/ui/top-header.vue
+++ Front/Project/components/ui/top-header.vue
@@ -1,25 +1,24 @@
 <template>
-  <header >
-    <logo></logo>
-  </header>
+	<header>
+		<logo></logo>
+	</header>
 </template>
 
 <script>
-import logo from "@/components/ui/logo";
+import logo from '@/components/ui/logo';
 
 export default {
-  name: "top-header",
-  components: {
-    logo
-  }
-}
+	name: 'top-header',
+	components: {
+		logo,
+	},
+};
 </script>
 
 <style lang="scss" scoped>
-header{
-  width: 100%;
-  background: #2ABBB6;
-  padding: 10px 20px;
-
+header {
+	width: 100%;
+	background: #2abbb6;
+	padding: 10px 20px;
 }
 </style>

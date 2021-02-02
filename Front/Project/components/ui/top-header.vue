b572accdf8d77efb91b6347320dcae144fb8b675
종훈 <leejonghun>
2021-02-02 14:08:22 +0900

    add haeder

---
+++ Front/Project/components/ui/top-header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header >
+    <logo></logo>
+  </header>
+</template>
+
+<script>
+import logo from "@/components/ui/logo";
+
+export default {
+  name: "top-header",
+  components: {
+    logo
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+header{
+  width: 100%;
+  background: #2ABBB6;
+  padding: 10px 20px;
+
+}
+</style>

552680628168e66eb1179d51ee2b6c06de0b10c8
종훈 <leejonghun>
2021-02-02 17:31:04 +0900

    add google map

--- Front/Project/nuxt.config.js
+++ Front/Project/nuxt.config.js
@@ -24,7 +24,10 @@ export default {
 	css: ['~assets/scss/mixin.scss'],
 
 	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-	plugins: [{ src: '~plugins/swiper', ssr: false }],
+	plugins: [
+		{ src: '~plugins/swiper', ssr: false },
+		{ src: '~plugins/googleMap', ssr: false },
+	],
 
 	// Auto import components: https://go.nuxtjs.dev/config-components
 	components: true,

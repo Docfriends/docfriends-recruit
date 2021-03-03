c491043ad43e1dda5902e08060fea0b7e987d0f0
김규빈 <9bin08@naver.com>
2021-03-03 12:12:27 +0900

    Done: 알고리즘 테스트 완료

--- Server/Algorithm/algorithm.js
+++ Server/Algorithm/algorithm.js
@@ -1,1 +1,21 @@
-const algorithm = (q) => {};
+const algorithm = (q) => {
+  const numbers = [];
+  let sliceNumber = "";
+
+  for (let i = 0; i < q.length; i++) {
+    if (!isNaN(q[i])) {
+      sliceNumber += q[i];
+    } else {
+      sliceNumber && numbers.push(sliceNumber);
+      sliceNumber = "";
+    }
+  }
+
+  const minIndex = numbers.indexOf(String(Math.min(...numbers)));
+  const maxIndex = numbers.indexOf(String(Math.max(...numbers)));
+  return minIndex + maxIndex;
+};
+const q =
+  "ab23c4d56e78f9g12h34i5j12k45l67n89m99o1k123p456q567r768s890t67u456v345w234x23y239z";
+
+console.log(algorithm(q));
